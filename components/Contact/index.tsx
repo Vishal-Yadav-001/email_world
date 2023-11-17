d01c4cd45328e2117107803ad8ab8c079da0196d
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2023-11-17 08:53:20 +0530

    new chnages

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,6 +1,36 @@
+"use client"
+import { useEffect } from "react";
 import NewsLatterBox from "./NewsLatterBox";
 
 const Contact = () => {
+  let submitted = false;
+
+  useEffect(() => {
+    //Runs only on the first render
+  }, [submitted]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    formData.append("access_key", "f683b8b3-d649-4f99-91f6-06edafefa02e");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    });
+    const result = await response.json();
+    if (result.success) {
+      submitted = true
+    }
+  }
+
   return (
     <section id="contact" className="  overflow-hidden  py-16 md:py-20 lg:py-28">
       <div className="container ">
@@ -12,11 +42,11 @@ const Contact = () => {
               "
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Need a new Email Signature? Open a Ticket
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
-                Please let me know if your queries I will try to reach out ASAP.              </p>
-              <form>
+                Please let me know if I can help getting you a new Email Signature, I will try to reach out ASAP.              </p>
+              <form onSubmit={handleSubmit}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
@@ -27,6 +57,8 @@ const Contact = () => {
                         Your Name
                       </label>
                       <input
+                        name="name"
+                        minLength={3}
                         type="text"
                         placeholder="Enter your name"
                         className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
@@ -42,6 +74,7 @@ const Contact = () => {
                         Your Email
                       </label>
                       <input
+                        name="email"
                         type="email"
                         placeholder="Enter your email"
                         className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
@@ -59,15 +92,22 @@ const Contact = () => {
                       <textarea
                         name="message"
                         rows={5}
+                        minLength={5}
                         placeholder="Enter your Message"
                         className="border-stroke dark:text-body-color-dark dark:shadow-two w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                       ></textarea>
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="shadow-submit dark:shadow-submit-dark rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90">
-                      Submit Ticket
-                    </button>
+                    {submitted ?
+                      <button className="shadow-submit dark:shadow-submit-dark rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90">
+                        Thanks I will reach you out soon
+                      </button>
+                      :
+                      <button className="shadow-submit dark:shadow-submit-dark rounded-sm bg-primary px-9 py-4 text-base font-medium text-white duration-300 hover:bg-primary/90">
+                        Submit Ticket
+                      </button>
+                    }
                   </div>
                 </div>
               </form>
